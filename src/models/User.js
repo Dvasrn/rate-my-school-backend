eedfd30affafc272a63dd63f459b6ad0722e44c2
Dvasrn <dawaasurenbazarragcha4@gmail.com>
2026-07-22 01:02:29 +0800

    fix(schema): update favoriteSchoolIds field to default to an empty array

--- src/models/User.js
+++ src/models/User.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     birthDate: { type: String, default: "" },
     schools: { type: [userSchoolSchema], default: [] },
+    favoriteSchoolIds: { type: [String], default: [] },
   },
   { versionKey: false }
 );
